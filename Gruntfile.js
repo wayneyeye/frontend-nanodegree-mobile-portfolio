f46f00fbd297533683dfab5873fd0d9cb03df5ec
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-02 13:31:02 -0600

    regrunted views

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
-          cwd: 'img/',
+          cwd: 'views/images/',
           dest: 'img_optmzd/'
         }]
       }
